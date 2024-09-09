613feed53c00c935f680cc4054363e4b0e74f569
DolphyXSTD <timofey31052008@gmail.com>
2024-09-10 01:45:12 +0300

    Speech rec from now

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("YEY it's first commit")
+import speech_recognition as spr
+import vosk
+
+sr = spr.Recognizer()
+sr.pause_threshold = 0.5
+
+with spr.Microphone() as mic:
+    sr.adjust_for_ambient_noise(source=mic, duration=0.5)
+    audio = sr.listen(mic)
+    query = sr.recognize_vosk(audio_data=audio, language="ru-RU").lower()
+
+    print(query)
