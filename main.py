7d3b853e9e1b64983582f16b2ea6ef018d3f581e
DolphyXSTD <timofey31052008@gmail.com>
2024-09-10 02:31:33 +0300

    Now many commands

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import speech_recognition as spr
 sr = spr.Recognizer()
 sr.pause_threshold = 0.5
 
-with spr.Microphone() as mic:
-    sr.adjust_for_ambient_noise(source=mic, duration=0.5)
-    audio = sr.listen(mic)
-    query = sr.recognize_vosk(audio_data=audio, language="ru-RU").lower()
+def listen_command():
+    with spr.Microphone() as mic:
+        sr.adjust_for_ambient_noise(source=mic, duration=0.5)
+        audio = sr.listen(mic)
+        query = sr.recognize_vosk(audio_data=audio, language="ru-RU").lower()
 
-    print(query)
+        print(query)
+
+while True:
+    listen_command()
